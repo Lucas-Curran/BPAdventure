35f36e4e4eeab26acbb678a05a1e73b8f7b9f0d8
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2021-12-28 03:21:40 -0500

    Added button options to text box

--- core/src/com/mygdx/game/TextBox.java
+++ core/src/com/mygdx/game/TextBox.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -20,15 +21,19 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.components.*;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
-public class TextBox {
+public class TextBox implements InputProcessor {
 	
 	private Stage stage;
 	private Table table;
@@ -36,7 +41,6 @@ public class TextBox {
 	private Group group;
 	private Image img;
 	private TextureRegion tex;
-	private BitmapFont font;
 	private int numChars;
 	private float timePerCharacter;
 	private float ctimePerCharacter;
@@ -52,12 +56,13 @@ public class TextBox {
 	private String[] originalText;
 	private Color color;
 	private int textSequence;
+	private boolean options;
+	private TextButton yesButton, noButton;
 	
 	public TextBox(BitmapFont font, Stage stage, Color color) {
-		this.font = font;
 		this.stage = stage;
 		this.color = color;
-		
+
 		table = new Table();
 		group = new Group();
 		
@@ -71,6 +76,22 @@ public class TextBox {
 		textureAtlas = new TextureAtlas("textures.txt");
 		tex = new TextureRegion(textureAtlas.findRegion("textbox2"));
 		img = new Image(tex);
+		
+		yesButton = new TextButton("Yes", Utilities.buttonStyles("default-rect", "default-rect-down"));
+		noButton = new TextButton("No", Utilities.buttonStyles("default-rect", "default-rect-down"));
+		
+		Utilities.buttonSettings(yesButton);
+		Utilities.buttonSettings(noButton);
+		
+		yesButton.getLabel().setStyle(new LabelStyle(font, null));
+		noButton.getLabel().setStyle(new LabelStyle(font, null));
+		
+		yesButton.getLabel().setFontScale(0.7f);
+		noButton.getLabel().setFontScale(0.7f);
+		
+		yesButton.setVisible(false);
+		noButton.setVisible(false);
+		
 		textAni = new TextureRegion(textureAtlas.findRegion("arrowAni"));
 		label = new Label("", new Label.LabelStyle(font, null));
 		label.setWrap(true);
@@ -86,6 +107,8 @@ public class TextBox {
 		
 		group.addActor(img);
 		group.addActor(label);
+		group.addActor(yesButton);
+		group.addActor(noButton);
 		
 		group.setScale(.5f);
 		
@@ -95,9 +118,19 @@ public class TextBox {
 		label.setPosition(img.getX() + widthOffset / 2, img.getY() + img.getHeight() - heightOffset);
 		img.setPosition(0, 0);
 		
+		yesButton.setPosition(70, 20);
+		noButton.setPosition(200, 20);
+		
+		yesButton.setHeight(40);
+		noButton.setHeight(40);
+		
+		yesButton.setWidth(120);
+		noButton.setWidth(120);
+		
 		textSequence = 0;
 		
 		table.setVisible(false);
+		options = true;
 	}
 	
 	public void renderTextBox(float delta) {
@@ -105,9 +138,23 @@ public class TextBox {
 			String stringText = originalText[textSequence].substring(0, numChars);
 			label.setColor(color);
 			label.setText(stringText);
-
+			
 			if (originalText[textSequence].length() == numChars) {
 				writing = false;
+				if (options) {
+					
+					if (noButton.isOver()) {
+						noButton.remove();
+						group.addActor(noButton);
+					} else if (yesButton.isOver()) {
+						yesButton.remove();
+						group.addActor(yesButton);
+					}
+					
+					yesButton.setVisible(true);
+					noButton.setVisible(true);
+					options = false;				
+				}
 			}
 
 			if (originalText[textSequence].length() > numChars) {
@@ -122,7 +169,7 @@ public class TextBox {
 			stage.act(delta);
 			stage.draw();
 
-			if (!writing) {
+			if (!writing && !yesButton.isVisible() && !noButton.isVisible()) {
 				elapsedTime += Gdx.graphics.getDeltaTime();
 				batch.begin();
 				batch.draw(anim.getKeyFrame(elapsedTime, true), img.getX(Align.center) - 30, label.getHeight() + 8);
@@ -135,6 +182,10 @@ public class TextBox {
 		stage.dispose();
 		batch.dispose();
 	}
+
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height);
+	}
 	
 	public boolean isWriting() {
 		return writing;
@@ -142,6 +193,10 @@ public class TextBox {
 	
 	public void setWritingSpeed(float timePerCharacter) {
 		this.timePerCharacter = timePerCharacter;
+	}
+	
+	public TextBox getInstance() {
+		return this;
 	}
 	
 	public void hideTextBow() {
@@ -165,6 +220,12 @@ public class TextBox {
 		table.setVisible(true);
 	}
 	
+	public void setOptions(boolean options, String confirmText, String rejectText) {
+		this.options = options;
+		yesButton.setText(confirmText);
+		noButton.setText(rejectText);
+	}
+	
 	public String[] getText() {
 		return text;
 	}
@@ -183,4 +244,67 @@ public class TextBox {
 		timePerCharacter = 0.045f;
 	}
 	
+	public Stage getStage() {
+		return stage;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if (yesButton.isPressed()) {
+			table.setVisible(false);
+			yesButton.setVisible(false);
+			noButton.setVisible(false);
+			Map.getInstance().getLevels().getLevelOne().getShopWindow().setShopVisible(true);
+			return true;
+		} else if (noButton.isPressed()) {
+			table.setVisible(false);
+			yesButton.setVisible(false);
+			noButton.setVisible(false);
+			System.out.println("no");
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float amountX, float amountY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
