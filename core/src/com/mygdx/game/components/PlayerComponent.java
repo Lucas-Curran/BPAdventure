7b4d9b5b130c580139bc2274561cba8e2e666f29
Wersdgs <682935@sylvaniastudents.org>
2022-01-05 13:40:57 -0500

    Fix merge issue pt 2

--- core/src/com/mygdx/game/components/PlayerComponent.java
+++ core/src/com/mygdx/game/components/PlayerComponent.java
@@ -5,4 +5,9 @@ import com.badlogic.ashley.core.Component;
 public class PlayerComponent implements Component {
 	public boolean player = false;
 
+	public final int LEFT = 1;
+	public final int RIGHT = 2;
+
+	public int direction = 0;
+
 }
