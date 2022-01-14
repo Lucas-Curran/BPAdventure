f266a347a705e6856146812f43b847e98cd4e518
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2022-01-13 21:18:13 -0500

    Add crash writer for crash logging

---
+++ core/src/com/mygdx/game/CrashWriter.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CrashWriter {
+	
+	private FileWriter fw;
+	private PrintWriter pw;
+	Exception error;
+	
+	public CrashWriter(Exception error) throws IOException {
+		this.error = error;
+		fw = new FileWriter("CrashReport.txt", true);
+		pw = new PrintWriter(fw);
+	}
+	
+	public void writeCrash() throws IOException {
+		pw.print(new SimpleDateFormat("[yyyy.MM.dd.HH:mm:ss]").format(new Date()));
+		error.printStackTrace(pw);
+		pw.close();
+		fw.close();
+	}
+	
+}
